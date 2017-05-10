5aa7633f1cd701d2d2ff8dfa3dc051e8f374603b
Alex Ivanov <a.i.babkin@yandex.ru>
2017-05-10 22:15:05 +0300

    'require' instead of import for srcSet images

--- src/Main.js
+++ src/Main.js
@@ -4,14 +4,8 @@ import './Main.css';
 //import images
 import logo from './img/logo.png';
 import shoes1Image from './img/adidas-shoes-1.jpg';
-  import shoes1X2Image from './img/adidas-shoes-1@2x.jpg';
-  import shoes1X3Image from './img/adidas-shoes-1@3x.jpg';
 import shoes2Image from './img/adidas-shoes-2.jpg';
-  import shoes2X2Image from './img/adidas-shoes-2@2x.jpg';
-  import shoes2X3Image from './img/adidas-shoes-2@3x.jpg';
 import shoes3Image from './img/adidas-shoes-3.jpg';
-  import shoes3X2Image from './img/adidas-shoes-3@2x.jpg';
-  import shoes3X3Image from './img/adidas-shoes-3@3x.jpg';
 
 export default () => {
   return (
@@ -62,7 +56,7 @@ export default () => {
           <div className="row">
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item sale">
-                <img src={shoes1Image} srcSet={shoes1X2Image + ' 2x, ' + shoes1X3Image + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
+                <img src={shoes1Image} srcSet={require('./img/adidas-shoes-1@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-1@3x.jpg') + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$170</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -70,7 +64,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item sale">
-                <img src={shoes2Image} srcSet={shoes2X2Image + ' 2x, ' + shoes2X3Image + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
+                <img src={shoes2Image} srcSet={require('./img/adidas-shoes-2@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-2@3x.jpg') + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$240.99</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -78,7 +72,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item">
-                <img src={shoes3Image} srcSet={shoes3X2Image + ' 2x, ' + shoes3X3Image + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
+                <img src={shoes3Image} srcSet={require('./img/adidas-shoes-3@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-3@3x.jpg') + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$1024</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -88,7 +82,7 @@ export default () => {
           <div className="row">
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item">
-                <img src={shoes2Image} srcSet={shoes2X2Image + ' 2x, ' + shoes2X3Image + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
+                <img src={shoes2Image} srcSet={require('./img/adidas-shoes-2@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-2@3x.jpg') + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$170</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -96,7 +90,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item">
-                <img src={shoes3Image} srcSet={shoes3X2Image + ' 2x, ' + shoes3X3Image + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
+                <img src={shoes3Image} srcSet={require('./img/adidas-shoes-3@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-3@3x.jpg') + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$230</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -104,7 +98,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item sale">
-                <img src={shoes1Image} srcSet={shoes1X2Image + ' 2x, ' + shoes1X3Image + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
+                <img src={shoes1Image} srcSet={require('./img/adidas-shoes-1@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-1@3x.jpg') + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$190</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -115,7 +109,7 @@ export default () => {
           <div className="row">
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item sale">
-                <img src={shoes1Image} srcSet={shoes1X2Image + ' 2x, ' + shoes1X3Image + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
+                <img src={shoes1Image} srcSet={require('./img/adidas-shoes-1@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-1@3x.jpg') + ' 3x'} alt="Adidas Shoes 1" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$170</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -123,7 +117,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item sale">
-                <img src={shoes2Image} srcSet={shoes2X2Image + ' 2x, ' + shoes2X3Image + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
+                <img src={shoes2Image} srcSet={require('./img/adidas-shoes-2@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-2@3x.jpg') + ' 3x'} alt="Adidas Shoes 2" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$240.99</a>
                 <span className="sale-label">Sale</span>
               </div>
@@ -131,7 +125,7 @@ export default () => {
 
             <div className="col-xs-12 col-lg-4">
               <div className="catalog-item">
-                <img src={shoes3Image} srcSet={shoes3X2Image + ' 2x, ' + shoes3X3Image + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
+                <img src={shoes3Image} srcSet={require('./img/adidas-shoes-3@2x.jpg') + ' 2x, ' + require('./img/adidas-shoes-3@3x.jpg') + ' 3x'} alt="Adidas Shoes 3" className="item-image" />
                 <a href="/item" title="Single item page" className="item-link">$1024</a>
                 <span className="sale-label">Sale</span>
               </div>
