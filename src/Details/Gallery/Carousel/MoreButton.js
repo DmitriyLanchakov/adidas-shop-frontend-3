cc87d12ad736983ce828bf44c380a09c9d5fc45d
Alex Ivanov <a.i.babkin@yandex.ru>
2017-05-15 11:32:18 +0300

    fix more button styles

--- src/Details/Gallery/Carousel/MoreButton.js
+++ src/Details/Gallery/Carousel/MoreButton.js
@@ -4,25 +4,21 @@ import media from '../../../styles/media';
 const Button = styled.button`
   display: none;
   ${media.tablet`
-    display: inline-block;
-  `}
+      display: block;
+      border: none;
+      background: none;
+      font-family: 'AvenirNextReg';
+      color: #c0c0c0;
+      font-size: 14px;
+      text-align: left;
+      cursor: pointer;
+      width: 64px;
+      flex: 0 1 40px;
+
+      &:hover {
+        color: #6e6e6e;
+      }
+    `}
 `;
 
 export default Button;
-
-/*  ${media.tablet`
-    display: block;
-    border: none;
-    background: none;
-    font-family: 'AvenirNextReg';
-    color: #c0c0c0;
-    font-size: 14px;
-    text-align: left;
-    cursor: pointer;
-    width: 64px;
-    flex: 0 1 40px;
-
-    &:hover {
-      color: #6e6e6e;
-    }
-  `}*/
